3d163b0d9b5c38fdcc9fdae4dc51603e30956636
Jerika Kanz <jerikacapila@gmail.com>
2022-03-05 14:01:51 +0100

    Adding Data

---
+++ Project/main.js
@@ -0,0 +1,71 @@
+const dataObj = {
+  "2022-02": [
+    {
+      project_name: "Project 1",
+      "A. Kanz": 11.0,
+      "J. Kanz": 0.0,
+      "R. Kanz": 8.0,
+      Summe: 19.0,
+    },
+    {
+      project_name: "Project 2",
+      "A. Kanz": 0.0,
+      "J. Kanz": 14.0,
+      "R. Kanz": 7.0,
+      Summe: 21.0,
+    },
+    {
+      project_name: "Project 3",
+      "A. Kanz": 5.0,
+      "J. Kanz": 8.0,
+      "R. Kanz": 3.0,
+      Summe: 16.0,
+    },
+  ],
+  "2022-01": [
+    {
+      project_name: "Project 1",
+      "A. Kanz": 9.0,
+      "J. Kanz": 0.0,
+      "R. Kanz": 10.0,
+      Summe: 21.0,
+    },
+    {
+      project_name: "Project 2",
+      "A. Kanz": 0.0,
+      "J. Kanz": 14.0,
+      "R. Kanz": 7.0,
+      Summe: 17.0,
+    },
+    {
+      project_name: "Project 3",
+      "A. Kanz": 5.0,
+      "J. Kanz": 8.0,
+      "R. Kanz": 3.0,
+      Summe: 20.0,
+    },
+  ],
+  "2021-12": [
+    {
+      project_name: "Project 1",
+      "A. Kanz": 9.0,
+      "J. Kanz": 0.0,
+      "R. Kanz": 10.0,
+      Summe: 17.0,
+    },
+    {
+      project_name: "Project 2",
+      "A. Kanz": 0.0,
+      "J. Kanz": 14.0,
+      "R. Kanz": 7.0,
+      Summe: 15.0,
+    },
+    {
+      project_name: "Project 3",
+      "A. Kanz": 5.0,
+      "J. Kanz": 8.0,
+      "R. Kanz": 3.0,
+      Summe: 18.0,
+    },
+  ],
+};
